6b57314efc3d9e1378dce346e4f76cd4210c7fb5
Semih Gokol <gokol.semih@gmail.com>
2022-06-12 10:53:45 +0200

    ajout de react-icon et mise en place du css pour implémenter la suppression d'Item

--- src/Component/ItemListe.js
+++ src/Component/ItemListe.js
@@ -1,11 +1,16 @@
 import '../style/itemListe.css'
+import { BsTrash } from 'react-icons/bs'
+
 
 function ItemListe(props) {
     return (
         //on récupère les props id et name qu'on va utiliser pour notre affichage
-        <li id={props.id}>
+        <li id={props.id} className='ligneItem'>
+            <input type="checkbox"/>
             <div className='divItem'>{props.itemName}</div>
-        </li>
+            <button onClick={() => props.delFunc(props.id)} 
+            className="btn btn-danger p-2 h-50"><BsTrash/></button>
+            </li>
 
     )
 }
