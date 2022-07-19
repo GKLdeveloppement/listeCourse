8ed04bc1051ac578a46fce030ce563aa1ceb9aca
Semih Gokol <gokol.semih@gmail.com>
2022-07-19 22:22:42 +0200

    ajout d'un élément card pour implémenter les éléments retournant de l'API dedans

--- src/Component/Fruits.js
+++ src/Component/Fruits.js
@@ -13,9 +13,18 @@ export default function Fruits() {
         console.log(dataJson.data);
     });
 
+    //Faire un for pour parcourir chaque itération dans l'objet JSON afin d'afficher une card pour chacun d'entre eux
     return (
-        <div>
-            
-        </div>
-    )
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      );
 }
